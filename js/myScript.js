fd9cee99df279d7536f26db28351111386ee4722
Márta Kapocs <marta.kapocs@gmail.com>
2020-08-08 04:50:37 +0200

    Improving the files of the Hamburger-place-webpage once again.

---
+++ js/myScript.js
@@ -0,0 +1,25 @@
+let smokeyApplewood = 1790;
+let sajtburger = 1190;
+let tijuanaBurger = 1790;
+let bbqBurger = 1590;
+let carnivoreBurger = 2190;
+let westCoast = 1790;
+let chickunaBurger = 1790;
+let surfAndTurf = 2190;
+let sweetHomeAvocado = 1990;
+let sayCheese = 1790;
+let chipotle = 1790;
+let burgerTower = 3390;
+let doubleDecker = 3390;
+
+let hamburgerTorta = 13900;
+
+let amountInput = document.querySelector("input[name='amountInput']");
+
+function total(price, amount){
+    alert(price*amount);
+}
+let sum = total(2190, 10);
+console.log(sum);
+
+
